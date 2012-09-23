d3e7b49c189216baf16e95d59dcf3cc0f0452407
Toda Takahiko <poepoemix@hotmail.com>
2012-09-23 19:23:17 +0900

    MediaManagerの変更にともなった修正。

--- src/com/ttProject/streaming/hls/HlsManager.java
+++ src/com/ttProject/streaming/hls/HlsManager.java
@@ -3,9 +3,20 @@ package com.ttProject.streaming.hls;
 import org.w3c.dom.Node;
 
 import com.ttProject.streaming.MediaManager;
+import com.ttProject.xuggle.ConvertManager;
 
 public class HlsManager extends MediaManager {
 	public HlsManager(Node node) {
 		super(node);
+	}
+	@Override
+	public boolean setup() {
+		return true;
+	}
+	@Override
+	public boolean resetupContainer() {
+		ConvertManager convertManager = ConvertManager.getInstance();
+		String url = HlsHandlerFactory.DEFAULT_PROTOCOL + ":" + convertManager.getName() + "_" + getName();
+		return resetupContainer(url, "mpegts");
 	}
 }
--- src/com/ttProject/streaming/webm/WebMManager.java
+++ src/com/ttProject/streaming/webm/WebMManager.java
@@ -3,9 +3,20 @@ package com.ttProject.streaming.webm;
 import org.w3c.dom.Node;
 
 import com.ttProject.streaming.MediaManager;
+import com.ttProject.xuggle.ConvertManager;
 
 public class WebMManager extends MediaManager {
 	public WebMManager(Node node) {
 		super(node);
+	}
+	@Override
+	public boolean setup() {
+		return true;
+	}
+	@Override
+	public boolean resetupContainer() {
+		ConvertManager convertManager = ConvertManager.getInstance();
+		String url = WebMHandlerFactory.DEFAULT_PROTOCOL + ":" + convertManager.getName() + "_" + getName();
+		return resetupContainer(url, "webm");
 	}
 }
