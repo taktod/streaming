029be260bb25838d732215b05a646748c50f34fb
Toda Takahiko <poepoemix@hotmail.com>
2012-09-29 09:23:55 +0900

    デコード実行部作成完了。

--- src/com/ttProject/xuggle/Transcoder.java
+++ src/com/ttProject/xuggle/Transcoder.java
@@ -3,6 +3,9 @@ package com.ttProject.xuggle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.ttProject.xuggle.flv.FlvManager;
+import com.xuggle.xuggler.IPacket;
+
 /**
  * Xuggleでメディアデータのコンバートを実行します。
  * ただし動作は大幅に変更するかも・・・
@@ -12,8 +15,48 @@ import org.slf4j.LoggerFactory;
 public class Transcoder implements Runnable {
 	/** ロガー */
 	private final Logger logger = LoggerFactory.getLogger(Transcoder.class);
-	
+	/** 動作定義 */
+	private volatile boolean keepRunning = true;
 	@Override
 	public void run() {
+		try {
+			// 読み込み用のコンテナをオープン
+			ConvertManager convertManager = ConvertManager.getInstance();
+			FlvManager flvManager = convertManager.getFlvManager();
+			flvManager.openInputContainer();
+			// 変換を実行する。
+			transcode();
+		}
+		catch (Exception e) {
+		}
+		finally {
+			logger.info("処理がおわりました。");
+			// 全処理を閉じる。
+			closeAll();
+		}
+	}
+	/**
+	 * 停止します。
+	 */
+	public void close() {
+		keepRunning = false;
+	}
+	/**
+	 * 全動作を止めます。
+	 */
+	private void closeAll() {
+		// コンテナとか閉じる(変換処理が完了したときに動作します。)
+	}
+	/**
+	 * 変換の中枢動作
+	 */
+	private void transcode() {
+		ConvertManager convertManager = ConvertManager.getInstance();
+		FlvManager flvManager = convertManager.getFlvManager();
+		IPacket packet = IPacket.make();
+		while(keepRunning) {
+			// コンバートを開始する。
+			keepRunning = flvManager.execute(packet);
+		}
 	}
 }
